f41bd9912ec779a53534ec0f69461c8442b0d807
unknown <milanzec35@gmail.com>
2023-06-08 19:07:02 +0200

    initial skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { SiMaterialUi } from 'react-icons/si';
+
+
+const skills = [
+  { name: 'HTML', icon: faHtml5 },
+  { name: 'CSS', icon: faCss3 },
+  { name: 'JavaScript', icon: faJs },
+  { name: 'React', icon: faReact },
+  { name: 'Material UI', icon: SiMaterialUi },
+];
 
 const Skills = () => {
   return (
     <div>
-        <p>Skills</p>
+      <h1>Skills</h1>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>
+            <FontAwesomeIcon icon={skill.icon} />
+            <span>{skill.name}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
