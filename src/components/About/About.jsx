971574903e0d6a546e751a75d91d2f62e90de5ff
unknown <milanzec35@gmail.com>
2023-06-08 18:35:47 +0200

    about initial

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 
+import { Box, Typography } from '@mui/material';
+
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-      <p>This is information about me and my background.</p>
-    </div>
-  )
-}
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Typography variant="h4" mt={2}>About Me</Typography>
+      <img
+        src="https://picsum.photos/300"
+        alt="Dummy"
+        style={{ width: '200px', borderRadius: '50%' }}
+      />
+      
+      <Typography variant="body1" mt={2}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo sed sapien
+        ullamcorper tincidunt. Etiam convallis vestibulum ex, sit amet ullamcorper metus
+        consectetur eu. Duis vestibulum urna at mauris luctus, vel bibendum lorem efficitur.
+      </Typography>
+    </Box>
+  );
+};
 
-export default About
+export default About;
+
+
+
+
